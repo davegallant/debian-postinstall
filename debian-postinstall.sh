faa0e0984e9a4a3a52619444990bfb8e9754a9c4
Dave Gallant <dave.gallant@esentire.com>
2019-09-01 22:30:12 -0400

    dump

---
+++ debian-postinstall.sh
@@ -0,0 +1,55 @@
+
+
+# Basic building block tools
+sudo apt install -y \
+    curl \
+    wget \
+    git
+
+# Developer tools
+sudo apt install -y \
+    git \
+    kitty \
+    shellcheck \
+    tmux \
+    vim
+
+# Networking tools
+sudo apt install -y \
+    dnsutils
+
+# Printer
+sudo apt install -y \
+    cups
+
+# Visuals
+sudo apt install -y \
+    fonts-firacode \
+    ttf-ubuntu-font-family
+
+# zsh
+if [ ! -e $HOME/.oh-my-zsh ]; then
+    sudo apt install -y zsh
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+fi
+
+# docker
+if [ $(docker --help) != 0 ]; then
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+    sudo apt-key fingerprint 0EBFCD88
+    sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/debian \
+    $(lsb_release -cs) \
+    stable"
+    sudo apt-get update
+    sudo apt-get install docker-ce docker-ce-cli containerd.io
+    sudo groupadd docker
+    sudo gpasswd -a $USER docker
+fi
+
+# Media
+sudo apt install -y \
+    deluge \
+    vlc
