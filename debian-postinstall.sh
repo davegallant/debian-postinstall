25f0aaaa8f75afbcf8ff055c2646d6a4d34d1a61
Dave Gallant <davegallant@gmail.com>
2020-02-29 20:45:18 -0500

    Remove spaceship prompt

--- debian-postinstall.sh
+++ debian-postinstall.sh
@@ -61,8 +61,6 @@ sudo apt install -y \
 if [ ! -e $HOME/.oh-my-zsh ]; then
     sudo apt install -y zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
-    ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
 fi
 
 # docker
